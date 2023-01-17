ea4ab03d84ae2b97cb75198836c3c8f306de0b5d
sranvare07 <88300754+sranvare07@users.noreply.github.com>
2023-01-17 10:04:31 +0530

    Added chrome coin flip extension

---
+++ README.md
@@ -0,0 +1,1 @@
+# chrome-coin-flip-extension
---
+++ script.js
@@ -0,0 +1,19 @@
+function tossCoin() {
+  var coinContainer = document.getElementById("coin-container");
+  document.querySelector(".result-text").innerHTML = "";
+  coinContainer.classList.add("coin-animation-class");
+  setTimeout(() => {
+    coinContainer.classList.remove("coin-animation-class");
+    var randNum = Math.random() * 10;
+    randNum = randNum.toFixed();
+    if (randNum % 2 == 0) {
+      document.querySelector(".result-text").innerHTML = "You got tails!";
+      coinContainer.style.transform = "rotateY(180deg)";
+    } else {
+      document.querySelector(".result-text").innerHTML = "You got heads!";
+      coinContainer.style.transform = "rotateY(0deg)";
+    }
+  }, 1500);
+}
+
+document.getElementById("toss-btn").addEventListener("click", tossCoin);
